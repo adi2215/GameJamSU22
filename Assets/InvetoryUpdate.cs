f62013755ec2fecdc5c233a48f1c3915b6147873
AnsarYesma <ansar.yesmukhanov@gmail.com>
2022-12-09 04:25:48 +0600

    shop&potions

---
+++ Assets/InvetoryUpdate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class InvetoryUpdate : MonoBehaviour
+{
+    public Data db;
+    public GameObject moneyCount;
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyCount.GetComponent<TextMeshProUGUI>().text = db.Money.ToString() + "$";
+        transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = db.Potion1.ToString();
+        transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = db.Potion2.ToString();
+        transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = db.Potion3.ToString();
+    }
+}
--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -11,4 +11,7 @@ public class Data : ScriptableObject
 
     public int Money = 10;
 
+    public int Potion1 = 0;
+    public int Potion2 = 0;
+    public int Potion3 = 0;
 }
